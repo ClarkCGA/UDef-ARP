152a1e40ca4c0cd7cd76dc20b67d127adac0cd9f
Yao-Ting <94820616+YaoTingYao@users.noreply.github.com>
2024-01-29 10:09:32 -0500

    Revised execute_workflow_cnf function for AR< 1.00001 dataset

--- allocation_tool.py
+++ allocation_tool.py
@@ -372,6 +372,7 @@ class AllocationTool(QObject):
         self.progress_updated.emit(75)
         # Set a maximum number of iterations to avoid infinite loop
         iteration_count = 0
+        new_prediction_density_arr = None
 
         # When AR > 1.00001 and iteration_count <= max_iterations, treat the result as new prediction density map to iterate the AR util AR is <=1.00001 or iteration_count = max_iterations
         while AR > 1.00001 and iteration_count <= max_iterations:
@@ -380,7 +381,9 @@ class AllocationTool(QObject):
             AR = self.calculate_adjustment_ratio_cnf(new_prediction_density_arr, deforestation_cnf)
             iteration_count += 1
         if iteration_count <= int(max_iterations):
-            out_ds_cnf = self.adjusted_prediction_density_map(new_prediction_density_arr, risk30_vp, AR, out_fn2)
+            selected_density_arr = new_prediction_density_arr if new_prediction_density_arr is not None else prediction_density_arr
+            out_ds_cnf = self.adjusted_prediction_density_map(selected_density_arr, risk30_vp, AR, out_fn2)
+
 
         else:
             print("Maximum number of iterations reached. Please reset the maximum number of iterations.")
